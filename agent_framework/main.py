185bd111c241065c2002fc4bcbe624ddc79b4b12
Ubuntu <ubuntu@HGX-001.maas>
2025-12-17 18:07:25 +0000

    update the stuff

--- agent_framework/main.py
+++ agent_framework/main.py
@@ -8,7 +8,8 @@ from beeai_framework.backend import ChatModel
 
 async def main():
     agent = RequirementAgent(
-        llm=ChatModel.from_name("ollama:granite3.3"),
+        #llm=ChatModel.from_name("ollama:granite3.3"),
+        llm = ChatModel.from_name("openai:ibm-granite/granite-4.0-h-small", base_url="http://localhost:8899/v1", api_key="PHD_KEY"),
         role="friendly AI assistant",
         instructions="Be helpful and conversational in all your interactions."
     )
