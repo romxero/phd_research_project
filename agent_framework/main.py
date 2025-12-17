10be9d644e052e82bdcb7b58a95e31721f8c29cd
Ubuntu <ubuntu@HGX-001.maas>
2025-12-17 20:25:45 +0000

    updated requirements for new tool calls for wikipedia and duckduckgo

--- agent_framework/main.py
+++ agent_framework/main.py
@@ -11,7 +11,7 @@ from beeai_framework.backend import ChatModel
 # the tools for the agent to be utilized
 from beeai_framework.tools.search.wikipedia import WikipediaTool
 from beeai_framework.tools.weather import OpenMeteoTool
-
+from beeai_framework.tools.search.duckduckgo import DuckDuckGoSearchTool
 
 async def main():
     agent = RequirementAgent(
@@ -19,10 +19,10 @@ async def main():
         llm = ChatModel.from_name("openai:ibm-granite/granite-4.0-h-small", base_url="http://localhost:8899/v1", api_key="PHD_KEY"),
         role="friendly AI assistant",
         instructions="Be helpful and conversational in all your interactions.",
-        tools=[WikipediaTool(), OpenMeteoTool()]
+        tools=[WikipediaTool(), OpenMeteoTool(), DuckDuckGoSearchTool()]
     )
 
-    response = await agent.run("What is the current weather in Tokyo?")
+    response = await agent.run("Generate a verbose summary of the city: San Francisco, CA.")
     print(response.last_message.text)
 
 if __name__ == "__main__":
