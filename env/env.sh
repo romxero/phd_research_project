4e77ca6fc97bea9b3bd7c655eee6b9ea1e6b18fd
Randy <romxero@hotmail.com>
2026-09-05 18:58:15 -0700

    chore: remove some variables that were stuck on by the former state of the project

--- env/env.sh
+++ env/env.sh
@@ -3,21 +3,23 @@
 # this is an environment file that helps with selecting the apropriate model and inference engine
 
 
-_INF_ENGINE="ollama" 
-_MODEL="llama3.2:latest"
-_AGENT_MODEL="llama3.2:latest"
-_RAG_MODEL="llama3.2:latest"
-_PROMPT_FILE="prompt.txt"
-_CONTEXT_FILE="context.txt"
-_OUTPUT_FILE="output.txt"
-_ERROR_FILE="error.txt"
-_LOG_FILE="log.txt"
-_DEBUG_FILE="debug.txt"
-_TRACE_FILE="trace.txt"
-_DEBUG_LEVEL="debug"
-_TRACE_LEVEL="trace"
-_DEBUG_LEVEL="debug"
+GIT_PAGER=cat
+
+#_INF_ENGINE="ollama" 
+#_MODEL="llama3.2:latest"
+#_AGENT_MODEL="llama3.2:latest"
+#_RAG_MODEL="llama3.2:latest"
+#_PROMPT_FILE="prompt.txt"
+#_CONTEXT_FILE="context.txt"
+#_OUTPUT_FILE="output.txt"
+#_ERROR_FILE="error.txt"
+#_LOG_FILE="log.txt"
+#_DEBUG_FILE="debug.txt"
+#_TRACE_FILE="trace.txt"
+#_DEBUG_LEVEL="debug"
+#_TRACE_LEVEL="trace"
+#_DEBUG_LEVEL="debug"
 
 
 
-export _INF_ENGINE _MODEL _PROMPT_FILE _CONTEXT_FILE _OUTPUT_FILE _ERROR_FILE _LOG_FILE _DEBUG_FILE _TRACE_FILE _DEBUG_LEVEL _TRACE_LEVEL
+#export _INF_ENGINE _MODEL _PROMPT_FILE _CONTEXT_FILE _OUTPUT_FILE _ERROR_FILE _LOG_FILE _DEBUG_FILE _TRACE_FILE _DEBUG_LEVEL _TRACE_LEVEL
